2f39aa9d864fbce6708f813620abd1725ed3fece
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-10-23 16:52:39 +0200

    add AppUser class and make it a default identity class for the whole app

--- BetterAddressBook/Data/ApplicationDbContext.cs
+++ BetterAddressBook/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BetterAddressBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BetterAddressBook.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
     }
 }
---
+++ BetterAddressBook/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Identity;
+
+namespace BetterAddressBook.Models;
+
+public class AppUser : IdentityUser
+{
+    [Required]
+    [DisplayName("First Name")]
+    [StringLength(50, ErrorMessage = "{0} must be between {2} and {1} characters long", MinimumLength = 2)]
+    public string? FirstName { get; set; }
+    
+    [Required]
+    [DisplayName("Last Name")]
+    [StringLength(50, ErrorMessage = "{0} must be between {2} and {1} characters long", MinimumLength = 2)]
+    public string? LastName { get; set; }
+    
+    [NotMapped] public string? FullName => $"{FirstName} {LastName}";
+}
