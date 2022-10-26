47ffca81773da6c59f749b67e160e30ff8dac520
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-10-26 14:27:07 +0200

    authorize Caegories and Contacts controllers

--- BetterAddressBook/Controllers/CategoriesController.cs
+++ BetterAddressBook/Controllers/CategoriesController.cs
@@ -1,168 +1,168 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BetterAddressBook.Data;
+using BetterAddressBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using BetterAddressBook.Data;
-using BetterAddressBook.Models;
 
-namespace BetterAddressBook.Controllers
+namespace BetterAddressBook.Controllers;
+
+[Authorize]
+public class CategoriesController : Controller
 {
-    public class CategoriesController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public CategoriesController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public CategoriesController(ApplicationDbContext context)
+    // GET: Categories
+    public async Task<IActionResult> Index()
+    {
+        var applicationDbContext = _context.Categories.Include(c => c.AppUser);
+        return View(await applicationDbContext.ToListAsync());
+    }
+
+    // GET: Categories/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null || _context.Categories == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Categories
-        public async Task<IActionResult> Index()
+        var categoryModel = await _context.Categories
+            .Include(c => c.AppUser)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (categoryModel == null)
         {
-            var applicationDbContext = _context.Categories.Include(c => c.AppUser);
-            return View(await applicationDbContext.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(categoryModel);
+    }
+
+    // GET: Categories/Create
+    public IActionResult Create()
+    {
+        ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
+        return View();
+    }
+
+    // POST: Categories/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] CategoryModel categoryModel)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var categoryModel = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoryModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(categoryModel);
-        }
-
-        // GET: Categories/Create
-        public IActionResult Create()
-        {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View();
-        }
-
-        // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] CategoryModel categoryModel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(categoryModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
-            return View(categoryModel);
-        }
-
-        // GET: Categories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var categoryModel = await _context.Categories.FindAsync(id);
-            if (categoryModel == null)
-            {
-                return NotFound();
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
-            return View(categoryModel);
-        }
-
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,Name")] CategoryModel categoryModel)
-        {
-            if (id != categoryModel.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(categoryModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryModelExists(categoryModel.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
-            return View(categoryModel);
-        }
-
-        // GET: Categories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var categoryModel = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoryModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(categoryModel);
-        }
-
-        // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Categories == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
-            }
-            var categoryModel = await _context.Categories.FindAsync(id);
-            if (categoryModel != null)
-            {
-                _context.Categories.Remove(categoryModel);
-            }
-            
+            _context.Add(categoryModel);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryModelExists(int id)
+        ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
+        return View(categoryModel);
+    }
+
+    // GET: Categories/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || _context.Categories == null)
         {
-          return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+            return NotFound();
         }
+
+        var categoryModel = await _context.Categories.FindAsync(id);
+        if (categoryModel == null)
+        {
+            return NotFound();
+        }
+
+        ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
+        return View(categoryModel);
+    }
+
+    // POST: Categories/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,Name")] CategoryModel categoryModel)
+    {
+        if (id != categoryModel.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(categoryModel);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryModelExists(categoryModel.Id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", categoryModel.AppUserId);
+        return View(categoryModel);
+    }
+
+    // GET: Categories/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || _context.Categories == null)
+        {
+            return NotFound();
+        }
+
+        var categoryModel = await _context.Categories
+            .Include(c => c.AppUser)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (categoryModel == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryModel);
+    }
+
+    // POST: Categories/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        if (_context.Categories == null)
+        {
+            return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+        }
+
+        var categoryModel = await _context.Categories.FindAsync(id);
+        if (categoryModel != null)
+        {
+            _context.Categories.Remove(categoryModel);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CategoryModelExists(int id)
+    {
+        return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
--- BetterAddressBook/Controllers/ContactsController.cs
+++ BetterAddressBook/Controllers/ContactsController.cs
@@ -1,11 +1,13 @@
 using BetterAddressBook.Data;
 using BetterAddressBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BetterAddressBook.Controllers;
 
+[Authorize]
 public class ContactsController : Controller
 {
     private readonly ApplicationDbContext _context;
