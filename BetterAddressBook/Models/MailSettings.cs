c257596e2b25b555f82788b365c64289938ff934
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-11-11 12:24:03 +0100

    add MailSettings model

---
+++ BetterAddressBook/Models/MailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BetterAddressBook.Models;
+
+public class MailSettings
+{
+    public string? Email { get; set; }
+    public string? EmailPassword { get; set; }
+    public string? EmailDisplayName { get; set; }
+    public string? EmailHost { get; set; }
+    public int EmailPort { get; set; }
+}
