e0d4fc6921ab6b50907df38861bb9ee3f70a8300
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-11-17 14:24:54 +0100

    add EmailCategoryViewModel

---
+++ BetterAddressBook/Models/ViewModels/EmailCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BetterAddressBook.Models.ViewModels;
+
+public class EmailCategoryViewModel
+{
+    public HashSet<ContactModel> Contacts { get; set; } = new();
+    public EmailData? EmailData { get; set; }
+    
+}
