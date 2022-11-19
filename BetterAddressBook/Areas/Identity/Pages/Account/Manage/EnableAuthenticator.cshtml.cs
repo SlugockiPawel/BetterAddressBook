68963216b2c57d5ce36f1dcdc265d67ef3214353
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-11-19 12:57:04 +0100

    display qr code using js library and enable 2fa authentication

--- BetterAddressBook/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ BetterAddressBook/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,9 +181,10 @@ namespace BetterAddressBook.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Contact Pro"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
     }
 }
+
